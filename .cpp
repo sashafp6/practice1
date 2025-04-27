5409f2dac69c46380f795aebd6f6b82029eebd77
sashafp6 <filippovaalexa@mail.ru>
2025-04-27 13:43:29 +0700

    Create .cpp

---
+++ .cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std; 
+//умножение
+int main() { 
+    int a, b, multi;
+    cout << "Введите первое целое число";
+    cin >> a;
+    cout << "Введите второе целое число:";
+    cin >> b;
+    multi = a*b;
+    cout << "Результат умножения" << a << "*" << b << "равен" << multi << endl;
+    return 0;
+} 
